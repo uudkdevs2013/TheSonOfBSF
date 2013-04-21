7191a0f1cf51649193b831f8aa4e31a8e75d57ab
schimdt.cpp <schmidt.cpp@gmail.com>
2013-04-21 18:17:21 -0500

    fixed the prefab name for the level load

--- BattleScaresFortress/Assets/Scripts/LevelLoader.cs
+++ BattleScaresFortress/Assets/Scripts/LevelLoader.cs
@@ -8,10 +8,10 @@ public class LevelLoader : MonoBehaviour
 	{
 		if (PhotonNetwork.isMasterClient)
 		{
-			PhotonNetwork.Instantiate("First Person Controller", new Vector3(-100, 100, 0), Quaternion.Euler(0, 0, 0), 0);
+			PhotonNetwork.Instantiate("FirstPersonController", new Vector3(1024, 100, 1024), Quaternion.Euler(0, 0, 0), 0);
 			for (int i = 0; i < PhotonNetwork.otherPlayers.Length; ++i)
 			{
-				PhotonNetwork.Instantiate("First Person Controller", new Vector3(i * 100, 100, 0), Quaternion.Euler(0, 0, 0), 0);
+				PhotonNetwork.Instantiate("FirstPersonController", new Vector3(i * 100 + 1024, 100, 1024), Quaternion.Euler(0, 0, 0), 0);
 			}
 		}
 	}
