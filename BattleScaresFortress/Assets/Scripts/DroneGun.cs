fe98681ab5e8df82b2b802ed1677685924413bd7
Matheus Andrade <matheus.amazonas@gmail.com>
2013-04-28 18:31:28 -0500

    Added DroneGun and Drone scripts

---
+++ BattleScaresFortress/Assets/Scripts/DroneGun.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DroneGun : NetworkedComponent
+{
+	[SerializeField] private float _range = 50;
+	[SerializeField] private float _damage = 3;
+	[SerializeField] private float _inaccuracy = 0.25f;
+	[SerializeField] private float _fireInterval = 0.3f;
+	
+	[SerializeField] private Transform _firePoint;
+	[SerializeField] private AudioSource _fireSound;
+	[SerializeField] private GameObject _trail;
+	
+	private float _fireCounter;
+	private LinkedList<Vector3> _fireTargets = new LinkedList<Vector3>();
+	
+	public void UpdateFiring(float delta, bool fire)
+	{
+		if(fire)
+		{
+			if(_fireCounter <= 0.0f)
+			{
+				Fire();
+				_fireCounter += _fireInterval;
+			}
+		}
+		_fireCounter -= delta;
+		if(_fireCounter < 0.0f)
+			_fireCounter = 0.0f;
+	}
+	
+	public void Fire()
+	{
+		Vector3 direction = GetInaccuracy(_firePoint, _inaccuracy);
+		Vector3 hit = _firePoint.position + (_range * direction);
+		RaycastHit rH;
+		if(Physics.Raycast(new Ray(_firePoint.position, direction), out rH, _range))
+		{
+			// Handle damage
+			var player = rH.collider.gameObject.GetComponent<PlayerController>();
+			if (player != null)
+			{
+				player.ApplyDamage(_damage);
+			}
+			
+			hit = rH.point;
+			FireEffect(_firePoint, rH.point);
+		}
+		else
+		{
+			FireEffect(_firePoint, _firePoint.position + (_range * _firePoint.forward));
+		}
+		FireEffect(_firePoint, hit);
+		_fireTargets.AddLast(hit);
+		_fireSound.Play();
+	}
+	
+	private Vector3 GetInaccuracy(Transform source, float amount)
+	{
+		Vector3 result = source.forward;
+		result += (Random.Range(-1.0f, 1.0f) * amount) * source.up;
+		result += (Random.Range(-1.0f, 1.0f) * amount) * source.right;
+		return result.normalized;
+	}
+	
+	public void FireEffect(Transform source, Vector3 target)
+	{
+		GameObject instance = (GameObject) GameObject.Instantiate(_trail);
+		BulletTrail trail = instance.GetComponent<BulletTrail>();
+		if(trail != null)
+		{
+			trail.Position(source.position, target);
+		}
+	}
+	
+	public override bool NeedsSend()
+	{
+		return _fireTargets.Count > 0;
+	}
+	
+	public override void SendData(PhotonStream stream, PhotonMessageInfo info)
+	{
+		stream.SendNext(_fireTargets.First.Value);
+		_fireTargets.RemoveFirst();
+	}
+	
+	public override void ReadData(PhotonStream stream, PhotonMessageInfo info)
+	{
+		Vector3 target = (Vector3) stream.ReceiveNext();
+		FireEffect(_firePoint, target);
+		_fireSound.Play();
+	}
+}
---
+++ BattleScaresFortress/Assets/Scripts/_Enemy Movement/Drone.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Drone : MonoBehaviour
+{
+	
+	[SerializeField] PhotonView _photonView;
+	
+	// Hover-related 
+	[SerializeField]
+	private float desiredHeight;									// The desired height the object should hover
+	private float hoverHeight;										// The current hover height
+	[SerializeField]
+	private float hoverVelocity;									// The hover velocity 
+	
+	// Player following-related
+	[SerializeField]
+	private float maxVelocity;										// Max following velocity
+	[SerializeField]
+	private float minDistance;										// Minimun distance to keep from player
+	private float distanteToPlayer;									// Current distance to player
+	private float velocity;											// Current following velocity
+	
+	[SerializeField] private DroneGun _gun;
+	[SerializeField] float firingDistance = 25.0f;
+	[SerializeField] float fireInterval;
+	float fireCounter;
+	
+	
+	// Target
+	[SerializeField]
+	private GameObject target = null;
+	
+	// Update is called once per frame
+	private void Update()
+	{
+		if (target == null)
+		{
+			FindTarget();
+		}
+		else
+		{
+			maintainHeight();
+			followPlayer();
+			TryFire();
+		}
+	}
+	
+	private void FindTarget()
+	{
+		if (_photonView.isMine)
+		{
+			PlayerController closestPlayer = null;
+			float closestDistance = float.MaxValue;
+			foreach (var player in PlayerController.GetAllPlayers())
+			{
+				float distance = Vector3.Distance(player.transform.position, transform.position);
+				if (distance < closestDistance)
+				{
+					closestPlayer = player;
+					closestDistance = distance;
+				}
+			}
+			_photonView.RPC("rpcSetTarget", PhotonTargets.Others, closestPlayer.photonView.owner.name);
+			target = closestPlayer.gameObject;
+		}
+	}
+	
+	[RPC]
+	private void rpcSetTarget(string targetName)
+	{
+		foreach (var player in PlayerController.GetAllPlayers())
+		{
+			if (player.photonView.owner.name == targetName)
+			{
+				target = player.gameObject;
+				break;
+			}
+		}
+	}
+	
+	// Maintain desired height
+	private void maintainHeight()
+	{
+		float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
+		float targetHeight = target.transform.position.y;
+		if (terrainHeight < targetHeight)
+		{
+			hoverHeight = transform.position.y - targetHeight;
+		} else
+		{
+			hoverHeight = transform.position.y - terrainHeight;
+		}
+		
+		if (hoverHeight != desiredHeight)
+		{
+			Vector3 newPos = transform.position;	
+			newPos.y += (desiredHeight - hoverHeight) * hoverVelocity;
+			transform.position = newPos;
+		}
+	}
+	
+	// Making it follow the target
+	private void followPlayer()
+	{
+		transform.LookAt(target.transform); 
+		distanteToPlayer = Vector3.Distance(transform.position, target.transform.position);
+		velocity = distanteToPlayer - minDistance;
+		if (velocity > maxVelocity)
+		{
+			velocity = maxVelocity;
+		}
+		if (distanteToPlayer > minDistance)
+		{
+			transform.position += transform.forward * velocity * Time.deltaTime;
+		} else if (distanteToPlayer < minDistance)
+		{
+			//velocity = distanteToPlayer + minDistance;
+			transform.position += transform.forward * velocity * Time.deltaTime;
+		}
+
+	}
+	
+	void TryFire() {
+			if (distanteToPlayer <= firingDistance)
+			{
+				float delta = Time.deltaTime;
+				_gun.UpdateFiring(delta, true);
+			}
+	}
+}
