97ec9c402592f3c805b64e8ebec357293652883f
Matheus Andrade <matheus.amazonas@gmail.com>
2013-04-29 13:06:08 -0500

    Coding Standards to Drone.cs and removed Hover.cs

--- BattleScaresFortress/Assets/Scripts/Boundary.cs
+++ BattleScaresFortress/Assets/Scripts/Boundary.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
+// Boundary used to switch between AI an simple follow behavior.
+// Once the Crawler enters the Fortress area, it will enable IA (OnTriggerIn)
+// Once it exits, it will enable simple follow (OnTriggerExit)
 public class Boundary : MonoBehaviour {
 
 	void OnTriggerExit (Collider obj) {
