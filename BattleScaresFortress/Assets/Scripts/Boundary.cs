d031c2678344b9020b3c3a90df1ee1b92d8869ef
schimdt.cpp <schmidt.cpp@gmail.com>
2013-04-29 13:48:00 -0500

    added a null check

--- BattleScaresFortress/Assets/Scripts/Boundary.cs
+++ BattleScaresFortress/Assets/Scripts/Boundary.cs
@@ -3,38 +3,22 @@ using System.Collections;
 
 public class Boundary : MonoBehaviour
 {
-
-//	void OnTriggerExit(Collider obj)
-//	{
-//		GameObject o = obj.gameObject;
-//		if (o.GetComponent<AstarAI>())
-//		{
-//			o.SendMessage("SetInPath", false);
-//		}
-//		
-//	}
-//	
-//	void OnTriggerEnter(Collider obj)
-//	{
-//		GameObject o = obj.gameObject;
-//		if (o.GetComponent<AstarAI>())
-//		{
-//			o.SendMessage("SetInPath", true);
-//		}
-//	}
 	
 	[SerializeField] private Vector2 _centerPoint;
 	[SerializeField] private float _radius;
 	
 	private void Update()
 	{
-		foreach (var enemy in Enemy.AllEnemies())
+		if (Enemy.AllEnemies() != null)
 		{
-			var crawler = enemy.GetComponent<Crawler>();
-			if (crawler != null)
+			foreach (var enemy in Enemy.AllEnemies())
 			{
-				var crawlerPosition = new Vector2(crawler.transform.position.x, crawler.transform.position.z);
-				crawler.gameObject.SendMessage("SetInPath", Vector2.Distance(_centerPoint, crawlerPosition) <= _radius);
+				var crawler = enemy.GetComponent<Crawler>();
+				if (crawler != null)
+				{
+					var crawlerPosition = new Vector2(crawler.transform.position.x, crawler.transform.position.z);
+					crawler.gameObject.SendMessage("SetInPath", Vector2.Distance(_centerPoint, crawlerPosition) <= _radius);
+				}
 			}
 		}
 	}
